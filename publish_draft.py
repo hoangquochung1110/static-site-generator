b8300eb4f25c84b22ba745b0aa64c536bf6e3f89
hunghoang-saritasa <hung.hoang@saritasa.com>
2022-07-28 15:18:57 +0700

    Publish django-f-expression

---
+++ publish_draft.py
@@ -0,0 +1,17 @@
+import argparse
+import pathlib
+FileNotFoundError
+parser = argparse.ArgumentParser()
+parser.add_argument("article")
+args = parser.parse_args()
+
+def move_draft_to_srcs(article: str):
+    srcs_path = pathlib.Path("./srcs")
+    draft_path = pathlib.Path("./drafts")
+    try:
+        draft_article = draft_path / article
+    except FileNotFoundError:
+        print(f"No such file name in drafts directory: {article}")
+    draft_article.rename(srcs_path / article)
+
+move_draft_to_srcs(args.article)
