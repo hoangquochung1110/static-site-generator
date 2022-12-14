3de004c7c698f453358669c6c164264a1de3b5f5
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2022-12-14 22:25:11 +0700

    Publish Factory Method

--- publish_draft.py
+++ publish_draft.py
@@ -7,11 +7,12 @@ args = parser.parse_args()
 
 def move_draft_to_srcs(article: str):
     srcs_path = pathlib.Path("./srcs")
-    draft_path = pathlib.Path("./drafts")
+    root_path = pathlib.Path(".")
     try:
-        draft_article = draft_path / article
+        draft_article = root_path / article
     except FileNotFoundError:
         print(f"No such file name in drafts directory: {article}")
-    draft_article.rename(srcs_path / article)
+    draft_article.rename(srcs_path / draft_article.name)
 
 move_draft_to_srcs(args.article)
+# TODO: auto build after moving to /srcs/
